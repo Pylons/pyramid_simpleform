1bfe0ee21d0d44f09c499e16bd41343fe2e160bd
Steve Piercy <web@stevepiercy.com>
2017-04-29 01:48:28 -0700

    tell Sphinx where to find the source start file and other parameters for latex

--- docs/conf.py
+++ docs/conf.py
@@ -201,9 +201,9 @@ latex_font_size = '10pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('latexindex', 'pyramid.tex',
-   'The Pyramid Web Application Development Framework',
-   'Chris McDonough', 'manual'),
+  ('index', 'pyramid_simpleform.tex',
+   'pyramid\_simpleform',
+   'Dan Jacob', 'manual'),
     ]
 
 # The name of an image file (relative to this directory) to place at the top of
